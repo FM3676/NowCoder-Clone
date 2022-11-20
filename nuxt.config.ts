1d169452e95cab5a8bda5241585eb0e0ae9fe0a2
§ <1309346392@qq.com>
2022-11-20 13:08:27 +0800

    Install Element Plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import ElementPlus from "unplugin-element-plus/vite";
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  css: ["element-plus/dist/index.css"],
+  build: {
+    transpile: ["element-plus/es"],
+  },
+  vite: {
+    plugins: [ElementPlus()],
+  },
 });
