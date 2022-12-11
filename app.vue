374ba316f85547ccce1123b5462cc540b29026b7
§ <1309346392@qq.com>
2022-12-11 15:14:46 +0800

    Finish a basic footer

--- app.vue
+++ app.vue
@@ -3,6 +3,13 @@
     <!-- App -->
     <div class="min-h-screen">
       <NuxtPage :key="$route.fullPath" />
+      <footer class="mt-8 w-full py-12 bg-gray-800">
+        <p
+          class="text-gray-600 text-8xl text-center hover:text-gray-300 transition-all cursor-pointer"
+        >
+          N O W C O D E R
+        </p>
+      </footer>
     </div>
   </div>
 </template>
