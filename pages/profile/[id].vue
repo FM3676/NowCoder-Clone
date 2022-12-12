9844a594015e5df8457922114a817c64b923a63e
§ <1309346392@qq.com>
2022-12-12 23:50:08 +0800

    Profile Page Change Looking Button

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -7,8 +7,28 @@
         style="width: 1200px"
       >
         <main class="col-span-9">
-          <div class="rounded-lg p-4 bg-white">
+          <div class="rounded-lg py-4 px-8 bg-white">
+            <ul
+              class="flex gap-4 text-gray-300 text-lg mb-4 relative transition-all"
+            >
+              <li
+                v-for="(item, index) in ['帖子', '动态']"
+                :key="item"
+                @click="changeLookingFor(index)"
+                :class="[{ 'text-green-350': index === lookingFor }]"
+                class="hover:text-green-350 cursor-pointer text-center w-12"
+              >
+                {{ item }}
+              </li>
+              <div
+                class="absolute h-1 w-6 rounded-full bg-green-350 left-3 top-8 transition-all"
+                :style="{ transform: `translateX(${4 * lookingFor}rem)` }"
+              ></div>
+            </ul>
+            <UISpinner v-if="isLoading" class="relative left-1/2" />
+
             <ProfileUsersPost
+              v-if="!isLoading"
               :profile="profile!"
               :posts="postList?.posts!"
               :total="postList?.total!"
@@ -36,14 +56,22 @@ definePageMeta({
 const route = useRoute();
 const { getProfile } = useUsers();
 const { getPostList } = usePost();
+const lookingFor = ref(0);
+const isLoading = ref(true);
 const profile = ref({} as UserProfile);
 const postList = ref<{
   posts: Post[];
   total: number;
 }>();
+
+const changeLookingFor = (n: number) => {
+  lookingFor.value = n;
+};
 onMounted(async () => {
+  isLoading.value = true;
   profile.value = await getProfile(parseInt(route.params.id as string));
   postList.value = await getPostList(1, 10, profile.value.id);
+  isLoading.value = false;
 });
 </script>
 
