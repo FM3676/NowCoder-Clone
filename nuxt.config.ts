7812bc4acf8d6cf57fd98fa9716c5ae516a16797
§ <1309346392@qq.com>
2022-12-13 19:42:33 +0800

    Install VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import ElementPlus from "unplugin-element-plus/vite";
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@vueuse/nuxt", "@nuxtjs/tailwindcss"],
   css: ["element-plus/dist/index.css"],
   build: {
     transpile: ["element-plus/es"],
